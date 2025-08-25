06b439e4a8eea54caaa414d62eb81a2f0b691598
yum-yum03 <elbekaymariampro@gmail.com>
2025-08-25 17:35:49 +0100

    Create _app.tsx

---
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import ErrorBoundary from "@/components/ErrorBoundary";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
+}
+
+export default MyApp;
